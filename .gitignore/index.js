aed8b62491de6358e1efe26c9e1a59c9a7bffe8d
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-12-08 22:55:09 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -387,12 +387,9 @@ if(message.content === "~ping"){
   
   bot.on("message", msg => {
       if (msg.content.startsWith("~createchannel")) {
-        if(message.member.roles.find("name", "Habitués")){
          new TempChannel(msg);
           msg.channel.send("Channel créé !");
-        }else{
-          message.reply("Vous n'avez pas le rôle requis..")
-         
-        }}
+      }
+        
   });
 
