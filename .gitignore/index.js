6287046989ebc9da748274295752b22b9c383870
asce5772 <44125928+asce5772@users.noreply.github.com>
2018-12-09 14:31:00 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -404,14 +404,14 @@ if(message.content === "~ping"){
         if(!msg.member.hasPermission("MANAGE_CHANNELS")){
          msg.reply("Vous n'avez pas la permission.")
         }else{
-          if(msg.content.slice.length !== 2){
-            msg.reply("Merci d'utiliser correctement la commande avec ``~tempochannel <\"nom\"> <nombre>``.")
-            }else{
-              new TempChannel(msg);
+          if(msg.content.slice.length === 2){
+            new TempChannel(msg);
           var embed1 = new Discord.RichEmbed()
           .setDescription("***Channel créé avec succés !***")
           .setColor("#8800fc")
              msg.channel.sendEmbed(embed1).catch(err => console.log(err)); 
+            }else{
+              msg.reply("Merci d'utiliser correctement la commande avec ``~tempochannel <\"nom\"> <nombre>``.")
             }
           
         }}}
